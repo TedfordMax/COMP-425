11f1953e7923b7347d3096c081fdf6b19fa9df34
TedfordMax <mib6@wildcats.unh.edu>
2018-12-05 14:47:28 -0500

    up
    dated

---
+++ lab8.java
@@ -0,0 +1,14 @@
+public static double finalGrade(int[] labs, int [] exams)
+{
+    int labSum = 0;
+    int examSum = 0;
+    double labGrade;
+    double examGrade;
+
+    double labSum = 0;
+    for (int i=0; i<labs.length; i++)
+    {
+        labSum += labs[i];
+    }
+    labGrade = labSum / labs.length();
+        labSum = finalGrade()(int[]labs * .4)*(int[]exam * .6)/2)
